e07678cbde686658114c7354ed966deefb0e4027
JTZ <JTZ@ZLSOFT.CN>
2026-03-07 11:56:52 +0800

    Make MahjongYOLOv8n public and add availability attributes

--- IOSMaJiangAR/MahjongYOLOv8n.swift
+++ IOSMaJiangAR/MahjongYOLOv8n.swift
@@ -8,11 +8,12 @@
 import CoreML
 
 /// Model wrapper class for MahjongYOLOv8n
-final class MahjongYOLOv8n {
-    let model: MLModel
+@available(iOS 12.0, macOS 10.14, tvOS 12.0, watchOS 5.0, *)
+public final class MahjongYOLOv8n {
+    public let model: MLModel
 
     /// URL of the underlying .mlmodelc directory.
-    class var urlOfModelInThisBundle: URL {
+    public class var urlOfModelInThisBundle: URL {
         let bundle = Bundle.main
         let url = bundle.url(forResource: "MahjongYOLOv8n", withExtension: "mlmodelc")!
         return url
@@ -20,12 +21,12 @@ final class MahjongYOLOv8n {
 
     /// Construct MahjongYOLOv8n instance with an existing MLModel object.
     /// Usually the application does not use this initializer unless it makes a subclass of MahjongYOLOv8n.
-    init(model: MLModel) {
+    public init(model: MLModel) {
         self.model = model
     }
 
     /// Construct MahjongYOLOv8n instance by automatically loading the model from the app's bundle.
-    convenience init(configuration: MLModelConfiguration = MLModelConfiguration()) throws {
+    public convenience init(configuration: MLModelConfiguration = MLModelConfiguration()) throws {
         let bundle = Bundle.main
         guard let url = bundle.url(forResource: "MahjongYOLOv8n", withExtension: "mlmodelc") else {
             throw NSError(domain: "MahjongYOLOv8n", code: -1, userInfo: [NSLocalizedDescriptionKey: "Model file not found in bundle"])
